a1621784950acf63e7ae1e4c9504f2eff6422d38
Yuri Yamamoto <YuriY@yamamotoyuri-no-MacBook.local>
2015-07-01 11:36:26 +0900

    daburi

--- solver_yuri.py
+++ solver_yuri.py
@@ -11,63 +11,66 @@ def distance(city1, city2):
 
 def initcenters(centers,n,cities):
     for i in range(n):
-        centers = [random.uniform(0,1600.0)][random.uniform(0,900.0)]
-        #centers[i] = cities[i*n]
+        centers[i] = cities[i*n]
     return centers
 
 def calc_new_centers(group,n,N,cities):
-    newcenters = [[0 for k in range(2)] for i in range(n)]
-    while(group[l] != []):
-        group[l][
-    for t in range(N):
-        for i in range(n):
-            sum[i] += (group[i][])**2
-             
-    newcenters[i] = (sum[i] / n)  + centers[i]        
+    newcenters = [() for i in range(n)]
+    for i in range(n):
+        sum = [0 for i in range(2)]
+        for s in range(len(group[i])):
+            sum[0] += cities[group[i][s]][0]/(len(group[i]))
+            sum[1] += cities[group[i][s]][1]/(len(group[i]))
+        newcenters[i]=tuple(sum)
     return newcenters
 
 def make_group(centers,cities,n,N):
-    group =[[0] for i in range(n)]
-        for t in range(N):
-            min_dis = min(distance(centers[0],cities[t]))
-            group_num = 0
-            for l in range(n):
-                if(min_dis >min(distance(centers[l],cities[t])):
-                   min_dis = min(distance(centers[l],cities[t]))
-                   group_num = l;
-            group[group_num].append(t)
+    group =[[] for i in range(n)]
+    for t in range(N):
+        min_dis = 1000000
+        group_num = 0
+        for l in range(n):
+            if(min_dis >distance(centers[l],cities[t])):
+                min_dis = distance(centers[l],cities[t])
+                group_num = l;
+                group[group_num].append(t)
     return group
 
 
 def kmeans(cities):
     N = len(cities)    
     dist = [[0] * N for i in range(N)]
+   
     for i in range(N):
         for j in range(N):
             dist[i][j] = dist[j][i] = distance(cities[i], cities[j])
     n = (int)(math.sqrt(N))
     centers = [[0 for k in range(2)] for i in range(n)]
     centers = initcenters(centers,n,cities)
-    for i in range(n):
-        print centers
-    for s in range(100):
+
+    #for i in range(n):
+        #print (centers[i])
+    for s in range(10):
         group = make_group(centers,cities,n,N)
         newcenters = calc_new_centers(group,n,N,cities)
-        if(distance(centers,newcenters)<0.5):
-            break
-        cuenters = newcenters
-    return g
+        #print("newcenters")
+       # print(newcenters)
+        #if(distance(cities[centers],cities[newcenters])<0.5):
+         #   break
+        centers = newcenters
+    return group
     
 
 
 
-def solve(cities):
-    N = len(cities)
+def solve(group,cities):
+    grouptup = tuple(group)
+    N = len(grouptup)
     dist = [[0] * N for i in range(N)]
     for i in range(N):
         for j in range(N):
-            dist[i][j] = dist[j][i] = distance(cities[i], cities[j])
-    current_city = 0
+            dist[i][j] = dist[j][i] = distance(cities[grouptup[i]], cities[grouptup[j]])
+    current_city =0
     unvisited_cities = set(range(1, N))
     solution = [current_city]
     
@@ -82,13 +85,21 @@ def solve(cities):
         current_city = next_city
     return solution
 
+def solutionplus(group,cities):
+    solution = [0]
+    for i in range(len(group)):
+        solution += solve(group[i],cities)
+    return solution
+
 #def merge(solutions):
     
 
 if __name__ == '__main__':
     assert len(sys.argv) > 1
     #solution = solve(read_input(sys.argv[1]))
-    means = kmeans(read_input(sys.argv[1]))
-    solution = solve(means)
-   # solution += solve(means)
+    cities = read_input(sys.argv[1])
+    group = kmeans(cities)
+    print("group")
+    print(group)
+    solution = solutionplus(group,cities)
     print_solution(solution)
